2536ef5a0a4258e88dc479f83a5a1246ec83b404
sohail_rasool-786 <sohail.rasool@conovoinc.com>
2021-12-23 18:58:30 +0500

    Add components

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { IoMdAdd, IoMdSearch } from 'react-icons/io';
+
+const Navbar = ({ searchTerm, setSearchTerm, user }) => {
+  const navigate = useNavigate();
+
+  if (!user) return null;
+
+  return (
+    <div className='flex gap-2 md:gap-5 w-full mt-5 pb-7'>
+      <div className='flex justify-start items-center w-full px-2 rounded-md bg-white border-none oultline-none focus-within:shadow-sm'>
+        <IoMdSearch className='text-2xl ml-1' />
+        <input
+          type='text'
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder='search'
+          value={searchTerm}
+          onFocus={() => navigate('/search')}
+          className='p-2 w-full bg-white outline-none'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
