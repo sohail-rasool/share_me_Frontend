a5e919e8a24e4d795c2ec29a6829d071704d1bf0
sohail_rasool-786 <sohail.rasool@conovoinc.com>
2021-12-24 13:14:18 +0500

    Add create div

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,6 +23,21 @@ const Navbar = ({ searchTerm, setSearchTerm }) => {
           className='p-2 w-full bg-white outline-none'
         />
       </div>
+      <div className='flex gap-3'>
+        <Link to={`user-profile/${user?._id}`} className='hidden md:block'>
+          <img
+            src={user.image}
+            alt='user-img'
+            className='w-14 h-12 rounded-lg object-contain'
+          />
+        </Link>
+        <Link
+          to='create-pin'
+          className='bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center'
+        >
+          <IoMdAdd />
+        </Link>
+      </div>
     </div>
   );
 };
